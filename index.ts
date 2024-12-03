2351f8793b58402758fcc0bd3522be21ea022434
ingrid68456 <dianaingridtakacs@gmail.com>
2024-12-03 14:17:46 +0200

    day 1 part 2

--- index.ts
+++ index.ts
@@ -2,11 +2,10 @@ import { solutionDay1 } from "@/solutions";
 
 async function startSolution() {
     const d = new Date();
-    const day = d.getDate() < 10 ? "0" + d.getDate() : d.getDate().toString();  // Ensure day is a string
+    const day = d.getDate() < 10 ? "0" + d.getDate() : d.getDate().toString();
 
     try {
-        const solution = await solutionDay1();
-        console.log('Solution: ' + solution);
+       await solutionDay1();
     } catch (err) {
         console.error("Error occurred while getting solution:", err);
     }
--- src/solutions/01/solution.ts
+++ src/solutions/01/solution.ts
@@ -1,37 +1,70 @@
 import { readProblemDataByDay } from "../utils/fileInput";
 
-export async function solutionDay1(): Promise<number> {
-    let solution = -1;
+export async function solutionDay1(): Promise<void> {
     try {
         const inputData = await readProblemDataByDay("01");
         if (inputData) {
-            let leftArray: number[] = [],
-                rightArray: number[] = [];
-            let splited = inputData.split(/\r?\n/);
-            for (let input of splited) {
-                let [parsedLeft, parsedRight] = input.split(/\s+/);
-                if (parsedLeft && parsedRight) {
-                    leftArray.push(parseInt(parsedLeft));
-                    rightArray.push(parseInt(parsedRight));
-                }
-            }
-
-            const descendingSort = (a: number, b: number) => b - a;
-            leftArray.sort(descendingSort);
-            rightArray.sort(descendingSort);
-
-            let length = rightArray.length;
-
-            let totalDistance = 0;
-            for (let index = 0; index < length; index++) {
-                let diff = Math.abs(leftArray[index] - rightArray[index]);
-                totalDistance += diff;
-            }
-            return totalDistance;
+            partOne(inputData);
+            partTwo(inputData);
         }
     } catch (err) {
         console.log(err);
     }
+}
 
-    return solution;
+function partOne(inputData: string): void {
+    let leftArray: number[] = [],
+        rightArray: number[] = [];
+    let splited = inputData.split(/\r?\n/);
+    for (let input of splited) {
+        let [parsedLeft, parsedRight] = input.split(/\s+/);
+        if (parsedLeft && parsedRight) {
+            leftArray.push(parseInt(parsedLeft));
+            rightArray.push(parseInt(parsedRight));
+        }
+    }
+
+    const descendingSort = (a: number, b: number) => b - a;
+    leftArray.sort(descendingSort);
+    rightArray.sort(descendingSort);
+
+    let length = rightArray.length;
+
+    let totalDistance = 0;
+    for (let index = 0; index < length; index++) {
+        let diff = Math.abs(leftArray[index] - rightArray[index]);
+        totalDistance += diff;
+    }
+    console.log("Part One: " + totalDistance);
+}
+
+function incrementMapIfExists(map: Map<number, number>, key: number): void {
+    if (map.has(key)) {
+        map.set(key, map.get(key)! + 1);
+    } else {
+        map.set(key, 1);
+    }
+}
+function partTwo(inputData: string): void {
+    let similarityScore = 0;
+    let splited = inputData.split(/\r?\n/);
+
+    let leftMap = new Map<number, number>(),
+        rightMap = new Map<number, number>();
+    for (let input of splited) {
+        let [parsedLeft, parsedRight] = input.split(/\s+/);
+        let left = parseInt(parsedLeft);
+        let right = parseInt(parsedRight);
+
+        incrementMapIfExists(leftMap, left);
+        incrementMapIfExists(rightMap, right);
+    }
+
+    for (let key of leftMap.keys()) {
+        if (rightMap.has(key)) {
+            similarityScore += key * (rightMap.get(key) ?? 0);
+        }
+    }
+
+    console.log("Part Two: " + similarityScore);
 }
